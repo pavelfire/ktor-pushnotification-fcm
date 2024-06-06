aaec03924a447c548532e9d8695d94018fcbb699
PVm116rus <fire_ra@bk.ru>
2024-06-06 09:31:33 +0300

    it's work if you put your valid service_account_key.json file

--- src/main/kotlin/com/vk/plugins/Routing.kt
+++ src/main/kotlin/com/vk/plugins/Routing.kt
@@ -2,7 +2,6 @@ package com.vk.plugins
 
 import com.vk.sendNotification
 import io.ktor.server.application.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
